004a438abb2046f126b54ae2868c48e8c2e8f4d8
Lei Peng <rockeet@163.com>
2014-12-05 21:55:38 +0800

    Update README.md

--- README.md
+++ README.md
@@ -2,3 +2,99 @@ nark-core
 =========
 
 Core utilities of nark library
+
+## Prerequisite
+  * boost-1.41 or newer
+  * `nark-bone` use boost as header-only library
+
+## Compile
+```bash
+$ cd /path/to/nark-bone
+$ make
+```
+
+## Components
+* [valvec](#valvec)
+* [Lexical Cast](#Lexical-Cast)
+* [Read text lines](#Read-text-lines)
+
+### valvec
+`nark::valvec` is a alternative to `std::vector`, it use `malloc`/`realloc`/`free`
+to manage memory. `Type` of `nark::valvec<Type>` must be [memmove-able](#memmove-able).
+
+valvec support [nark-serialization](https://github.com/rockeet/nark-serialization).
+
+### Fast expansion
+`valvec` is much faster than `std::vector` when expanding memory, when expanding,
+`std::vector` first allocate a larger memory, then copy-cons(move-cons in c++11) old
+objects, this is slow. But `valvec` just calling `realloc' to expand the memory, in
+virtual addressing system, for example:
+```c++
+   void* p = malloc(oldsize); // `p` is a virtual address
+   /// do something
+   void* q = realloc(p, newsize); // `q` is another virtual address
+```
+When available virtual address range pointed by `p` is not big enough,
+it is posible to just [remap](http://linux.die.net/man/2/mremap) the physical address of `p` into a larger
+free virtual address range. Most popular `malloc` implementations have used [remap](http://linux.die.net/man/2/mremap)
+to get this advantage.
+
+### low level functions
+  * risk_set_data, risk_set_size, risk_set_capacity
+  * risk_release_ownership
+
+[nark-fsa](https://github.com/rockeet/nark-fsa-intro) use these functions for
+supporting DFA memory map in an graceful way.
+
+### utility functions
+```c++
+   nark::valvec<int> vec;
+   vec.push(1); // `push` is an alias of `push_back`
+   vec.push(2); // expand when full
+   //...
+   vec.reserve(vec.size() + 100);
+   vec.unchecked_push(123); // assert when full
+   int val = vec.pop_val(); // throws when empty
+   //...
+   while (!vec.empty()) {
+      int top = vec.unchecked_pop_val();
+      //...
+   }
+```
+
+### memmove-able
+When memmove an object `x` to another memory block `y` without destruct `x`,
+if `y` can be used as it is `x`, then `x` is memmove-able.
+
+For example, gnu `std::list,map,set`... are not memmove-able, because they
+have self-references.
+
+### Lexical Cast
+Both compiling and running are much fast than `boost::lexical_cast`.
+```c++
+#include <febird/lcast.hpp>
+
+//...
+	int    i = nark::lcast("123");
+	long   l = nark::lcase(std::string("1") + "23");
+	float  f = nark::lcast("1.23");
+	double d = nark::lcast("1.23");
+```
+
+### Read text lines
+```c++
+#include <febird/util/linebuf.hpp>
+
+//...
+	nark::LineBuf line;
+	nark::valvec<fstring> fields; // fstring didn't own memory
+	while (line.getline(stdin) > 0) {
+		line.chomp(); // like perl chomp
+		line.split(&fields);
+		if (fields.size()) {
+			double dval = nark::lcast(fields[0]);
+			//...
+		}
+	}
+```
+
---
+++ src/nark/lcast.cpp
@@ -0,0 +1,194 @@
+//#define _XOPEN_SOURCE 600
+//#define _ISOC99_SOURCE
+
+#include "lcast.hpp"
+#include <stdexcept>
+#include <stdio.h>
+#include <stdlib.h>
+#include <boost/current_function.hpp>
+
+#ifdef __CYGWIN__
+	#define strtold strtod
+	#define strtof  strtod
+#endif
+
+#ifdef _MSC_VER
+	#define strtoll     _strtoi64
+	#define strtoull    _strtoui64
+#endif
+
+namespace nark {
+
+lcast_from_str::operator short() const {
+       	return atoi(p);
+}
+
+lcast_from_str::operator unsigned short() const {
+	char* q;
+	long l = strtoul(p, &q, 10);
+	if (q == p) {
+		throw std::invalid_argument("bad lcast");
+	}
+	return (unsigned short)l;
+}
+
+lcast_from_str::operator int() const {
+	return atoi(p);
+}
+
+lcast_from_str::operator unsigned() const {
+	char* q;
+	long l = strtoul(p, &q, 10);
+	if (q == p) {
+		throw std::invalid_argument("bad lcast");
+	}
+	return l;
+}
+
+lcast_from_str::operator long() const {
+	char* q;
+	long l = strtol(p, &q, 10);
+	if (q == p) {
+		throw std::invalid_argument("bad lcast");
+	}
+	return l;
+}
+
+lcast_from_str::operator unsigned long() const {
+	char* q;
+	unsigned long l = strtoul(p, &q, 10);
+	if (q == p) {
+		throw std::invalid_argument("bad lcast");
+	}
+	return l;
+}
+
+lcast_from_str::operator long long() const {
+	char* q;
+	long long l = strtoll(p, &q, 10);
+	if (q == p) {
+		throw std::invalid_argument("bad lcast");
+	}
+	return l;
+}
+
+lcast_from_str::operator unsigned long long() const {
+	char* q;
+	unsigned long long l = strtol(p, &q, 10);
+	if (q == p) {
+		throw std::invalid_argument("bad lcast");
+	}
+	return l;
+}
+
+lcast_from_str::operator float() const {
+	char* q;
+	float l = strtof(p, &q);
+	if (q == p) {
+		throw std::invalid_argument("bad lcast");
+	}
+	return l;
+}
+
+lcast_from_str::operator double() const {
+	char* q;
+	double l = strtod(p, &q);
+	if (q == p) {
+		throw std::invalid_argument("bad lcast");
+	}
+	return l;
+}
+
+lcast_from_str::operator long double() const {
+	char* q;
+	long double l = strtold(p, &q);
+	if (q == p) {
+		throw std::invalid_argument("bad lcast");
+	}
+	return l;
+}
+
+#define CONVERT(format) { \
+	char buf[48]; \
+	sprintf(buf, format, x); \
+	return buf; \
+}
+
+std::string lcast(int x) CONVERT("%d")
+std::string lcast(unsigned int x) CONVERT("%u")
+std::string lcast(short x) CONVERT("%d")
+std::string lcast(unsigned short x) CONVERT("%u")
+std::string lcast(long x) CONVERT("%ld")
+std::string lcast(unsigned long x) CONVERT("%lu")
+std::string lcast(long long x) CONVERT("%lld")
+std::string lcast(unsigned long long x) CONVERT("%llu")
+std::string lcast(float x) CONVERT("%f")
+std::string lcast(double x) CONVERT("%f")
+std::string lcast(long double x) CONVERT("%Lf")
+
+//////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////
+// Hex Lexical cast
+
+std::string hexlcast(int x) CONVERT("%X")
+std::string hexlcast(unsigned int x) CONVERT("%X")
+std::string hexlcast(short x) CONVERT("%X")
+std::string hexlcast(unsigned short x) CONVERT("%X")
+std::string hexlcast(long x) CONVERT("%lX")
+std::string hexlcast(unsigned long x) CONVERT("%lX")
+std::string hexlcast(long long x) CONVERT("%llX")
+std::string hexlcast(unsigned long long x) CONVERT("%llX")
+
+namespace {
+static const signed char hex_tab[] = {
+-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+ 0,  1,  2,  3,  4,  5,  6,  7,  8,  9, -1, -1, -1, -1, -1, -1,
+-1, 10, 11, 12, 13, 14, 15, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+-1, 10, 11, 12, 13, 14, 15, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+};
+
+template<class Int>
+Int hex_int_from_str(const char* str, size_t len) {
+	Int x = 0;
+//	printf("%s: str=%.*s, len=%zd\n", BOOST_CURRENT_FUNCTION, int(len), str, len);
+	for (size_t i = 0; i < len; ++i) {
+		int c = (unsigned char)(str[i]);
+		signed char d = hex_tab[c];
+		if (-1 == d) {
+			break;
+		}
+		x <<= 4;
+		x |= d;
+	}
+	return x;
+}
+} // namespace
+
+#define HEX_FROM_STR(Int) \
+hexlcast_from_str::operator Int() const { \
+	return hex_int_from_str<Int>(p, n); \
+}
+
+HEX_FROM_STR(short)
+HEX_FROM_STR(unsigned short)
+HEX_FROM_STR(int)
+HEX_FROM_STR(unsigned int)
+HEX_FROM_STR(long)
+HEX_FROM_STR(unsigned long)
+HEX_FROM_STR(long long)
+HEX_FROM_STR(unsigned long long)
+
+} // namespace nark
+
